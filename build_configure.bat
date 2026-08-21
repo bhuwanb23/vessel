357f6d87d2f8c65b8a8e9265e7250227ef0511d0
bhuwanb23 <bhuwanseervi4567@gmail.com>
2026-08-21 20:37:04 +0530

    Add build_configure.bat for CMake configuration and build setup

---
+++ build_configure.bat
@@ -0,0 +1,9 @@
+@echo off
+call "C:\Program Files\Microsoft Visual Studio\18\Community\VC\Auxiliary\Build\vcvarsall.bat" amd64 >nul 2>&1
+cd /d D:\projects\software\local_llm\llama.cpp
+if exist build\CMakeCache.txt del build\CMakeCache.txt
+if exist build\CMakeFiles rmdir /s /q build\CMakeFiles
+set CUDA_PATH=C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v12.9
+set CC=cl
+set CXX=cl
+"C:\Program Files\Microsoft Visual Studio\18\Community\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin\cmake.exe" -G Ninja -B build -S . -DGGML_CUDA=ON -DCMAKE_CUDA_ARCHITECTURES=120 -DCMAKE_BUILD_TYPE=Release -DCMAKE_CUDA_COMPILER="%CUDA_PATH%\bin\nvcc.exe" -DCMAKE_CUDA_FLAGS="-allow-unsupported-compiler"
